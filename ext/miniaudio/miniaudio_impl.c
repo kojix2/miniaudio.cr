ccb14bf78b50edc18f935447e00628e3c9537d22
kojix2 <2xijok@gmail.com>
2025-10-02 13:21:37 +0000

    Add miniaudio implementation and build configuration

---
+++ ext/miniaudio/miniaudio_impl.c
@@ -0,0 +1,4 @@
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
+
+// Nothing else - miniaudio is a single-file library and the header contains the implementation
--- src/miniaudio/lib_miniaudio.cr
+++ src/miniaudio/lib_miniaudio.cr
@@ -1,5 +1,13 @@
 module Miniaudio
   @[Link("miniaudio")]
   lib LibMiniaudio
+    # Basic type aliases
+    alias MaResult = Int32
+    alias MaHandle = Pointer(Void)
+
+    fun ma_engine_init(config : MaHandle, engine : MaHandle) : MaResult
+    fun ma_engine_uninit(engine : MaHandle) : Void
+    fun ma_engine_play_sound(engine : MaHandle, filename : LibC::Char*, pSoundGroup : MaHandle) : MaResult
+    fun ma_engine_get_time_in_pcm_frames(engine : MaHandle) : UInt64
   end
 end
