72d438d1f553eeed9651e74a4880e07e34ec15e5
kojix2 <2xijok@gmail.com>
2025-10-04 00:59:21 +0900

    Generated binding

--- src/miniaudio.cr
+++ src/miniaudio.cr
@@ -4,11 +4,11 @@ module Miniaudio
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
   def self.version
-    LibMiniaudio.ma_version(out major, out minor, out revision)
+    LibMiniaudio.version(out major, out minor, out revision)
     {major, minor, revision}
   end
 
   def self.version_string
-    String.new(LibMiniaudio.ma_version_string)
+    String.new(LibMiniaudio.version_string)
   end
 end
